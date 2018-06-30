3f4c455c95a6b979d45467cfa9c3a046d80b1a3b
queensburybill <queensburybill@gmail.com>
2018-06-30 14:20:45 -0400

    added slider to front-page.php

--- front-page.php
+++ front-page.php
@@ -3,93 +3,52 @@
 	<!-- If you want to insert code from a separate file into this file, check out
 	this video at around min. 9 or 10: https://www.youtube.com/watch?v=e8nJMopiH2Q&t=3s -->
 	</div>
-	<div class="container-fluid container-fluid-macsc">
-	
-		<div class="row mb-4">
-		
-			<!-- Bootstrap carousel -->
-			<div id="macscCarousel" class="carousel slide" data-ride="carousel">
 
-				<!-- Wrapper for the slides -->
-				<div class="carousel-inner">
+		
+		<!-- BEGIN OWL CAROUSEL2 HTML SETUP -->
+		<div class="slider-wrapper cap-container">
+			<div class="slider-holder">
+				<div class="owl-carousel owl-theme" id="carousel">
 
 					<?php 
 
-						// WP_Query
-						// In the code in this comment, the maximum number of posts that will show will match the entry in the wp-admin dashboard under Settings>Reading>Blog-pages-show-at-most: __ Posts. If you want to designate a specific number of posts, ie: 5, use 'type=post&posts_per_page-5'. If you use "-1", then the number of posts will be infinite.
-						// $sliders = new WP_Query('type=posts');
+					$args = array(
+						'type' => 'post',
+						'posts_per_page' => -1,
+						'post_type' => 'slides'
+					);
 
-						// Another format would be...
-// $args = array( 'post_type' => 'product', 'posts_per_page' => 10 );
+					$sliders = new WP_Query($args);
 
-						$args = array(
-							'type' => 'post',
-							'posts_per_page' => -1,
-							'post_type' => 'slides'
-							// 'category_name' => 'slide'
-							// 'offset' => 1,   this exludes the first post
+					if ( $sliders -> have_posts() ):
 
-						);
-						$sliders = new WP_Query($args);
-// 
-						if ( $sliders -> have_posts() ):
+						$count = 0;
+						$bullets = '';
 
-							$count = 0;
-							$bullets = '';
-
-							while( $sliders -> have_posts() ): $sliders -> the_post(); ?>
-								
-								
-								<div class="item" style="background-image:url('<?php echo get_field( "image" ); ?>');">
-									<h2><?php echo get_field( "headline" ); ?></h2>
-									<h3><?php echo get_field( "subhead" ); ?></h3>
-
+						while( $sliders -> have_posts() ): $sliders -> the_post(); ?>
+							
+							<div class="item" style="background-image:url('<?php echo get_field( "image" ); ?>');">
+								<div class="cap-text">									
+									<h2 class="cap-title"><?php echo get_field( "headline" ); ?></h2>
+									<p class="cap-desc"><?php echo get_field( "subhead" ); ?></p>
 								</div>
+							</div>
+							<!-- Indicators -->
+							<!-- If you need to troubleshoot the indicators, the Italian guy does his tutorial at around 25:00 at this YouTube URL: https://www.youtube.com/watch?v=0UzwBRLfsbM -->
 
+						<?php $count++; endwhile;
 
-								<!-- <div class="carousel-item <?php if($count == 0): echo 'active'; endif; ?>">
-									<?php the_post_thumbnail('full'); ?>
-									<div class="carousel-caption d-none d-md-block">
-										<?php the_title( sprintf('<h2 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ),'</a></h2>' ); ?>
-										<p><?php the_content(); ?></p>
-									</div>
-								</div> -->
+					endif;
 
-								<?php $bullets .= '<li data-target="#macscCarousel" data-slide-to="'.$count.'" class="'; ?>
-
-                                <?php if($count == 0): $bullets .= 'active'; endif; ?>
-
-								<?php $bullets .=  '"></li>'; ?>
-
-							<?php $count++; endwhile;
-
-						endif;
-
-						wp_reset_postdata();
+					wp_reset_postdata();
 
 					?>
 
-					<!-- Indicators -->
-					<!-- If you need to troubleshoot the indicators, the Italian guy does his tutorial at around 25:00 at this YouTube URL: https://www.youtube.com/watch?v=0UzwBRLfsbM -->
-					<ol class="carousel-indicators">
-						<?php echo $bullets; ?>
-					</ol>
-
 				</div>
-				<!-- Controls -->
-				<a class="carousel-control-prev" href="#macscCarousel" role="button" data-slide="prev">
-					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="carousel-control-next" href="#macscCarousel" role="button" data-slide="next">
-					<span class="carousel-control-next-icon" aria-hidden="true"></span>
-					<span class="sr-only">Next</span>
-				</a>
-			</div> <!-- close carousel -->
-				
-		</div> <!-- close row -->
+			</div>
+		</div>
 
-	</div>
+	  <div class="clearfix"></div>
 	
 	<div class="container">	
 
